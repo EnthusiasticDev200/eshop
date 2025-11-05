f215edfa7d73a2324dc10bba14f9ab30f91a32b4
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-05 15:05:00 +0100

    added Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from "./components/Header/Header.js"
+import Home from "./components/Home/Home.js"
+
 
 function App() {
   return (
     <div className="App">
         <Header/> 
+        <Home/>
     </div>
   );
 }
