ec7d2504dd50ab2956dbd2858d5b563db05e69d1
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-05 23:54:07 +0100

    created product files and it's styling file

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,12 +2,30 @@ import React from 'react'
 import "./Product.css"
 
 
-
-
-function Product() {
+function Product({id, title, price, rating, image}) {
     return (
         <div>
+            <div className="product">
+                <div className="product__info">
+                    <p className='product__title'> Item: {title} </p>
+                    <p className="product__price">#{price}</p>
+                    <div className="product__rating">
+                        {/* create new Array, pass rating, fill it and mao each */}
+                       {Array(rating) 
+                       .fill()
+                       .map((_,i)=>{ 
+                          return <p key={i}>⭐</p>
+                       })}
+                    </div>
 
+                    <img src={image} alt="" className="product__image" /> <br/>
+                    <button className='product__basketLink'>
+                        <a href='default.asp'>
+                            Add to Basket
+                        </a>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
