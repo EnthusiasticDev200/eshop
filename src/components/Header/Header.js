8a980391f3c5b9ea2e1d243b71f96962af4ba75c
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-06 14:23:52 +0100

    added link to shop icon and basket icon section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,22 @@ import "./Header.css"
 import SearchIcon from '@mui/icons-material/Search';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from "react-router-dom"
+
 
 function Header(){
     return (
         <div className="parent__header">
                 {/* headers comp. logo&searchBar&Nav */ }
-            <div className="header__logo">
-                <StorefrontIcon  className="header__logoImage" fontSize="large" />
-                <h2 className='header__logo_Title'> eShop</h2>
-            </div>
+                
+                {/* path to home */}
+            <Link to="/" style={{ textDecoration : "none"}} >
+                <div className="header__logo">
+                    <StorefrontIcon  className="header__logoImage" fontSize="large" />
+                    <h2 className='header__logo_Title'> eShop</h2>
+                </div>
+            </Link>
+            
         
             <div className="header__searchBar">
                 <input type='text' className='header__searchInput' placeholder="search product"/>
@@ -31,10 +38,14 @@ function Header(){
                     <span className="nav__item__lineTwo">Shop</span>
                 </div>
 
-                <div className="nav__item">
-                    <ShoppingBasketIcon className="basketIcon" />
-                    <span className="nav__item__lineTwo">0</span>
-               </div>
+                {/* path to checkout */}
+                <Link to="/checkout" style={{ textDecoration : "none"}}>
+                    <div className="nav__item">
+                        <ShoppingBasketIcon className="basketIcon" />
+                        <span className="nav__item__lineTwo">0</span>
+                    </div>
+                </Link>
+                
         
             </div>
         </div>
@@ -43,34 +54,3 @@ function Header(){
 
 
 export default Header
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
