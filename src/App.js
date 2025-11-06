e73c1ff4c14321b3c7cdf305e49a045f190769df
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-06 13:25:43 +0100

    implemented router and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from 'react'
 import './App.css';
 import Header from "./components/Header/Header.js"
 import Home from "./components/Home/Home.js"
-
+import Checkout from './components/Checkout/Checkout.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <Header/> 
-        <Home/>
+        <Router>
+            <Routes>
+                  {/* checkout path */}
+                <Route path='/checkout' element={<Checkout/>} />
+                  
+                <Route path='/' element={<Header/> && <Home/>} />
+            </Routes>
+        </Router>
+        
     </div>
   );
 }
