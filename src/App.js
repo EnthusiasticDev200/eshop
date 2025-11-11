02bb829eae5602e9895ad45c3f55960755615d6b
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-11 11:49:00 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react'
-import './App.css';
-import Header from "./components/Header/Header.js"
-import Home from "./components/Home/Home.js"
-import Checkout from './components/Checkout/Checkout.js';
+import './App.css'
+import Home from "./components/Home/home"
+import Checkout from './components/Checkout/checkout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './usersAction/SignIn';
+//import {ContextProvider} from './context'; //jeph code
+// import { StateProvider } from './dataLayer/StateProvider';
+// import reducer, { initialState } from './dataLayer/reducer';
+
 
 function App() {
   return (
     <div className="App">
-        <Router>
-            <Routes>
-                  {/* checkout path */}
-                <Route path='/checkout' element={<Checkout/>} />
+        {/* <ContextProvider> */}
+       
+            <Router>
+              <Routes>
+                    {/* checkout path */}
+                  <Route path='/checkout' element={<Checkout/>} />
 
-                <Route path='/' element={<Home/>} />
-            </Routes>
-        </Router>
+                  <Route path='/' element={<Home/>} />
+                  <Route path='/signin' element={<SignIn/>} />
+              </Routes>
+          </Router>
+        {/* </ContextProvider> */}
         
+          
     </div>
   );
 }
