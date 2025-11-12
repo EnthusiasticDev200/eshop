0dd18f9d7630838a1e8dd30f09524d3e3582f1c5
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-13 00:09:15 +0100

    moved firebase config into src module

---
+++ src/firebaseAuth/authConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey : process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain : process.env.REACT_APP_AUTH_DOMAIN,
+    projectId : process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket : process.env.EACT_APP_FIREBASE_STORAGE_BUCKET,
+    messageSenderId : process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+    appId : process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId : process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
+}
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+const auth = getAuth(app)
+
+
+export { auth, db }
