e0cb4bb5bd494db45f3caa7d4383382a1f77e898
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-13 00:10:21 +0100

    added login and signup event handler

--- src/usersAction/SignIn.js
+++ src/usersAction/SignIn.js
@@ -3,12 +3,35 @@ import './SignIn.css'
 import { Link } from 'react-router-dom'
 import StorefrontIcon from '@mui/icons-material/Storefront';
 
-// import Header from '../components/Header/Header'; 
+import { db, auth } from "../firebaseAuth/authConfig"
 
 
 function SignIm (){
+    // useHistory hook ??
+    //const history = useHistory(); // from route history in App.js
     const [ email, setEmail ] = useState('')
-    const { password, setPassword } = useState('')
+    const [ password, setPassword ] = useState('')
+
+    // login logic
+    const logIn = e =>{
+        e.preventDefault()
+        const response = auth.signInWithEmailAndPassword( email, password)
+        console.log(" login response", response)
+        .then( response => alert("login successful") ) 
+        .catch( error => alert( error.message ) ) //display error
+    }
+
+    const signUp = e =>{
+        e.preventDefault();
+        
+        const response = auth.createUserWithEmailAndPassword( email, password )
+        .then( response => {
+            // if reg OK
+           if (response)  alert("reg OK ")//history.push("/") 
+        })
+        .catch( error => alert( error. message ))
+    }
+
     return (
         <div className="signin">
             <div className="signin__container">
@@ -30,11 +53,11 @@ function SignIm (){
                         <small className='remember__me'>
                             <input type="checkbox" /> Remember Me
                         </small> <br />
-                        <button type='submit'className='signIn__button'> Login in</button>
+                        <button type='submit'className='signIn__button' onClick={logIn}> Login in</button>
 
                         <div className="signUp__form">
                             <h5> Don't have an account? </h5>
-                            <a href=''className="create__account__link" target='_blank'>Create account</a> 
+                            <a href='default.asp'className="create__account__link" target='_blank' onClick={signUp}>Create account</a> 
                         </div>
                     </form>
                 </div>
