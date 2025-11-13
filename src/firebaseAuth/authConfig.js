10603099364917999b739a69680846908edaac99
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-13 22:49:43 +0100

    corrected typo

--- src/firebaseAuth/authConfig.js
+++ src/firebaseAuth/authConfig.js
@@ -6,17 +6,15 @@ const firebaseConfig = {
     apiKey : process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain : process.env.REACT_APP_AUTH_DOMAIN,
     projectId : process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket : process.env.EACT_APP_FIREBASE_STORAGE_BUCKET,
-    messageSenderId : process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+    storageBucket : process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId : process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
     appId : process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId : process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
 }
 
 const app = initializeApp(firebaseConfig)
 
-const db = getFirestore(app)
+export const db = getFirestore(app)
 
-const auth = getAuth(app)
+export const auth = getAuth(app)
 
-
-export { auth, db }
