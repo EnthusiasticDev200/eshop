0476c483e8d4878476881027985f1e61ff8b09a6
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-18 18:05:20 +0100

    added routes for image upload

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,18 +1,36 @@
 import express from "express"
 
-import { createProduct, getProduct, searchProductByTitle } from "../controller/product.js"
+import { 
+    createProduct, createProductImageUrl, deleteProduct, 
+    getAllProduct, getProduct, searchProductByTitle, updateProduct 
+} from "../controller/product.js"
 
+import multer from "multer"
+
+// create upload file and store the images there
+const upload = multer({ dest : "uploads/"})  
 
 const router = express.Router()
 
 
+
+
+// create product image url
+router.post("/product/create/imageurl", upload.single("file"), createProductImageUrl)
 
 //product route
 router.post("/product/create", createProduct)
 
 router.get("/product/:id", getProduct)
 
-router.get("/product/search", searchProductByTitle)
+router.get("/product", searchProductByTitle)
+
+router.get("/products", getAllProduct)
+
+router.delete("/product/:id", deleteProduct)
+
+router.patch("/product/:id/update", updateProduct)
+
 
 
 
