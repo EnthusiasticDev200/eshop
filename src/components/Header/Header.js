ed0c5064b02c7baf0800404c6cf15fb71e6918d4
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-21 12:46:56 +0100

    added getProduct event and styled UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,58 @@
-import React from "react"
+import React, { useState, useContext } from "react"
 import "./Header.css"
 import SearchIcon from '@mui/icons-material/Search';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useStateValue } from "../../dataLayer/StateProvider";
 
+import UserContext from "context/userContext";
+import SearchedProductContext from "context/searchedProductContext";
 
 function Header(){
     const [ {basket}, dispatch ] = useStateValue() // removed dispatch
+    const [ searchProduct, setSearchProduct] = useState('')
+
+    const { user } = useContext(UserContext) //use context with updaated info from SignIn comp
+
+    const { setProduct } = useContext(SearchedProductContext)
+
+    const navigate = useNavigate()
+
+
+    const getProduct = async(e) =>{
+        e.preventDefault()
+        if ( !searchProduct ){
+            alert ("your input is empty")
+            return
+        }
+        //hit db
+        const url = process.env.REACT_APP_GET_PRODUCT
+       
+        try{
+            const response = await fetch(`${url}`+`${searchProduct}`, {
+                method : 'GET'
+            })
+            console.log("getting product", response)
+            if ( response.status !== 200) return alert(" product not found")
+            const data = await response.json()
+            console.log("data", data)
+
+            //update global data set
+            setProduct({
+                id : data._id,
+                title : data.title,
+                price : data.price,
+                imageUrl : data.imageUrl
+            })
+            navigate('/search/product') 
+        }catch(error){
+            console.log("error pulling product", error.message)
+            alert("error fecthing product")
+        }
+    }
+    
+    
     return (
         <div className="parent__header">
                 {/* headers comp. logo&searchBar&Nav */ }
@@ -23,17 +67,31 @@ function Header(){
             
         
             <div className="header__searchBar">
-                <input type='text' className='header__searchInput' placeholder="search product"/>
-                <button>
-                    <SearchIcon className='header__searchIcon'/>
-                </button>
+                <form onSubmit={getProduct}>
+                    <input 
+                        type='text' 
+                        className='header__searchInput' 
+                        placeholder="search product"
+                        value ={searchProduct}
+                        onChange={(e)=> setSearchProduct(e.target.value)}/>
+                    <button
+                        type="submit"
+                        className="header__searchIcon__btn">
+                        <SearchIcon className='header__searchIcon'/>
+                    </button>
+                </form>
             </div>
 
             <div className="header__nav">
                 <div className="nav__item">
-                    <span className="nav__item__lineOne">Hello Guest</span>
+                    <span className="nav__item__lineOne">{
+                        //display username if logged in
+                        user ? `Hello ${user.username}` : "Hello Guest"
+                        }</span>
                     <Link to="signin" style={{ textDecoration : "none"}}>
-                        <span className="nav__item__lineTwo">Sign In</span>
+                        <span className="nav__item__lineTwo">
+                            { user ? <>Sign Out</> : <>Sign In</> }
+                        </span>
                     </Link>
                     
                 </div>
